a4bed4ff762839fea8bb9e460b21fa345b135fa6
zw <zaiweixiong@Zaiweis-MacBook-Air.local>
2020-11-09 14:59:22 +1100

    added

---
+++ MDfiles/Data structures and Algorithms with Java Python and Javascript/Python/Stack.py
@@ -0,0 +1,17 @@
+class Stack:
+
+    def __init__(self):
+        self.items=[]
+    def push (self,item):
+        self.items.append(item)
+    def pop (self):
+        return self.items.pop()
+    def is_empty(self):
+        return self.items==[]
+    def size(self):
+        return len(self.items)
+    def peek(self):
+        return self.items[-1]
+    
+    def __str__(self):
+        return str(self.items)
