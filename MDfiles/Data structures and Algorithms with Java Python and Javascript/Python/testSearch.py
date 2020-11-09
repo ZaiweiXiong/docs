c230baacd5025ad7f1d79faaf83ca3ee6021184b
zw <zaiweixiong@Zaiweis-MacBook-Air.local>
2020-11-09 16:09:27 +1100

    update

---
+++ MDfiles/Data structures and Algorithms with Java Python and Javascript/Python/testSearch.py
@@ -0,0 +1,71 @@
+import pytest
+from random import randint
+#pytest testSearch.py::test_search -s
+
+
+def test_search():
+    nums = generatorNumbers()#[1,-1,9,10,100,2]
+    print(nums)
+    print('max=>',findMin(nums))
+
+def findMax(nums):
+
+    if len(nums)==1:
+        return nums[0]
+    op1 = nums[0]
+    #print('op1=>',op1)
+    op2 = findMax(nums[1:])
+    #print('op2=>',op2)
+
+    if op1>op2:
+        return op1
+    else:
+        return op2
+
+def findMin(nums):
+    min = nums[0]
+    for x in range(1,len(nums)):
+        if nums[x]<min:
+            min = nums[x]
+    return min
+
+def search(item,nums):
+
+    for i in range(0,len(nums)):
+        if nums[i]==item:
+            return nums[i]
+
+
+def bsort(arr):
+    for i in range(len(arr)-1,0,-1):
+        for j in range(i):
+            if arr[j]<arr[j+1]:
+                temp    = arr[j+1]
+                arr[j+1]= arr[j]
+                arr[j] = temp
+    return arr
+
+def binarySearch(item,nums):
+
+    print('binarySearch!')
+    n = len(nums)-1
+    lowerIdex=0
+    upperIdx=n
+
+    while lowerIdex <= upperIdx:
+        midPt = (lowerIdex+upperIdx)//2
+        if item == nums[midPt]:
+            return midPt
+        if item >nums[midPt]:
+            lowerIdex=midPt+1
+        else:
+            upperIdx=midPt-1
+
+    if lowerIdex>upperIdx:
+        return None
+
+def generatorNumbers():
+    nums=[]
+    for _ in range(10):
+        nums.append(randint(0, 100))
+    return nums
