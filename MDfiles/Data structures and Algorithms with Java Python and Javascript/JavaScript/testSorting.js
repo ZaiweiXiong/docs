7ea6bb0cd388081de413859c87971a582af2df55
zw <zaiweixiong@Zaiweis-MacBook-Air.local>
2020-12-02 12:27:18 +1100

    updatedfiles

--- MDfiles/Data structures and Algorithms with Java Python and Javascript/JavaScript/testSorting.js
+++ MDfiles/Data structures and Algorithms with Java Python and Javascript/JavaScript/testSorting.js
@@ -1,117 +1,109 @@
 console.log('this is test sorting!');
 // Sorting Algorithm
-// bubble sort
+// bubble sorting bsort
+// insert sorting inserSort
+// select sorting sSort
+// quick sorting  qSort
 
-var arr=[1,0,9,2,7,6,2,5]
+var arr= tArray();
+//console.log('=>'+arr);
+
 function bsort(arr){
 
     for (var i=0;i<arr.length;i++){
 
         for (var j=0;j<arr.length-i-1;j++){
+              
+                 if (arr[j]>arr[j+1]){
 
-              if (arr[j]>arr[j+1]){
-                   var temp = arr[j];
-                   arr[j]=arr[j+1];
-                   arr[j+1]=temp;
-              }
+                     var temp = arr[j+1];
+                     arr[j+1] = arr[j];
+                     arr[j] = temp;
+                 }
         }
     }
             return arr;
 }
 
-arr = bsort(arr);
-console.log('bubble sorting=>',arr);
-
-function findSameNumbers(arr){
-
-    for (var i=0;i<arr.length;i++){
-        for (var j=i+1;j<arr.length;j++){
-            if (arr[i]==arr[j]){
-                    console.log('local is '+j+' number '+arr[j]);
-            }
-        }
-    }
-}
-arr = bsort(arr);
-findSameNumbers(arr);
-
-//selctionSort
-var arr=[1,0,9,2,7,6,2,5]
-
 function sSort(arr){
+
     for (var i=0;i<=arr.length-2;i++){
         var min = i;
         for (var j=i+1;j<=arr.length-1;j++){
-            if (arr[min]>arr[j]){
+
+                 if(arr[min]>arr[j]){
+                     var temp = arr[min];
+                     arr[min] = arr[j];
+                     arr[j] = temp;
+                 }
+        }
+    }
+            return arr;
+}
+
+function ss(arr){
+    
+    for (var i=0;i<arr.length-1;i++){
+        var min = i;
+        for (j=i+1;j<arr.length;j++){
+            if (arr[j]<arr[min]){
                 var temp = arr[min];
                 arr[min] = arr[j];
                 arr[j] = temp;
-             }     
+            }
         }
-    }
-                return arr
-}
-console.log("selectSort=> "+sSort(arr));
-
-function inserSort(arr){
-    var n = arr.length;
-
-    for (var i=1;i<n;i++){
-        var key = arr[i];
-        var j=i-1;
-        while (j>=0&&arr[j]>key){
-            arr[j+1]=arr[j];
-            j=j-1;
-        }
-        arr[j+1]=key;
-    }
-        return arr;
-}
-
-function insertionSort(arr){
-    
-    var temp, inner;
-
-    for (var outer=1;outer<=arr.length-1;outer++){
-        temp = arr[outer];
-        inner = outer;
-        while(inner>0&&arr[inner-1]>=temp){
-            arr[inner]=arr[inner-1];
-            --inner;
-        }
-        arr[inner]=temp;
     }
     return arr;
 }
 
-// quick sorting
+function inserSort(arr){
+
+    var temp, i;
+    for (var j=1;j<arr.length;j++){
+        temp = arr[j];
+        i = j;
+        while(i>=0&&arr[i-1]>=temp){
+            arr[i]=arr[i-1];
+            --i;
+        }
+          arr[i]=temp;
+    }
+          return arr;
+}
 
 function qSort(arr){
-    
-    if (arr.length==0){
+
+    if (arr.length==0){ 
         return [];
     }
-    var left=[]
-    var right=[]
-    var pivot = arr[0]
+    var left = [];
+    var right= [];
+    var p    = arr[0];
     for (var i=1;i<arr.length;i++){
-        if (arr[i]<pivot){
-            left.push(arr[i])
+
+        if (arr[i]<p){
+            left.push(arr[i]);
         }else{
-            right.push(arr[i])
+            right.push(arr[i]);
         }
     }
-    return qSort(left).concat(pivot,qSort(right));
+    return qSort(left).concat(p,qSort(right));
 }
 
 
 function tArray(){
     var a=[]
-    for (var i=0;i<100;i++){  
+    for (var i=0;i<1000;i++){  
         a[i]=Math.floor((Math.random()*100)+1);
     }
      return a;
 }
+
+var start = new Date().getTime();
+ss(tArray());
+var stop = new Date().getTime();
+var elapsed = stop - start;
+console.log('ss spending time..'+elapsed/1000 +' secs')
 
 
 var start = new Date().getTime();
@@ -125,7 +117,6 @@ insertionSort(tArray());
 stop = new Date().getTime();
 elapsed = stop - start;
 console.log('insertionSort spending time..'+elapsed/1000 +' secs')
-
 
 start = new Date().getTime();
 qSort(tArray());
